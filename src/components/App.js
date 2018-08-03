f96c0c3530c1cdd3ad995318f665dc46bf0cd8f2
Meaghan Carrieres <meaghan.carriers@ontario.ca>
2018-08-02 20:13:35 -0400

    added logo to fix broken tests

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 
 import GoogleMapsComponent from "./MapContainer";
 import Button from "./Button";
+import logo from "../logo.svg";
 import "../App.css";
 
 class App extends Component {
