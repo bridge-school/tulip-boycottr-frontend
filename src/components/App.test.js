8fe1a88912badf77363d0494062fabc5d815c1e0
Jesse <jesse.david@live.ca>
2018-08-07 20:27:00 -0400

    added necessary imports to make test pass

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import configureStore from '../store/configureStore';
+import {Provider} from 'react-redux';
 
 const store = configureStore();
 
