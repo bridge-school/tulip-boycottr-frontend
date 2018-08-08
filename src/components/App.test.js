8730b838ec8659f3fe309caad7530850e0f56104
Jesse <jesse.david@live.ca>
2018-08-07 20:23:09 -0400

    added store to test file

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -2,8 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
+const store = configureStore();
+
 it("renders without crashing", () => {
   const div = document.createElement("div");
-  ReactDOM.render(<App healthCheck={() => {}} />, div);
+  ReactDOM.render(
+  <Provider store={store}>
+    <App healthCheck={() => {}} />
+  </Provider>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
